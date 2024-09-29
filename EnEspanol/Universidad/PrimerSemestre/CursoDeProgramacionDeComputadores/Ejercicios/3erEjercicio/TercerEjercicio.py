b0ed80437fc41d5a05c4fff956c55d50e66f66c9
Nicolas Alexander Herrera Rincon <124278805+N-A13XAND3R@users.noreply.github.com>
2024-09-29 15:32:20 -0500

    Create TercerEjercicio.py

---
+++ EnEspanol/Universidad/PrimerSemestre/CursoDeProgramacionDeComputadores/Ejercicios/3erEjercicio/TercerEjercicio.py
@@ -0,0 +1,13 @@
+def lineal_sort(arr):
+    for i in range(len(arr)):
+        num = arr[0]
+        for j in range(len(arr)-i):
+            if arr[j] < num:
+                num = arr[j]
+        arr.remove(num)
+        arr.append(num)
+    print(arr)
+
+lista = [123,214,3,2,22,3,32,-2,0]
+
+lineal_sort(lista)
