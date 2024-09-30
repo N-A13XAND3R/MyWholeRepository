f47511c03f5bd087002d363a1063c7d251b63859
Nicolas Alexander Herrera Rincon <124278805+N-A13XAND3R@users.noreply.github.com>
2024-09-29 19:39:22 -0500

    Create tortuga.js

---
+++ EnEspanol/Universidad/SegundoSemestre/pooCurso/Ejercicios/Blockly/turtle/tortuga.js
@@ -0,0 +1,153 @@
+//Solucion al primer ejercicio.
+for (var count = 0; count < 4; count++) {
+  moveForward(100);
+  turnRight(90);
+}
+
+//Solucion al segundo ejercicio.
+for (var count = 0; count < 5; count++) {
+  moveForward(100);
+  turnRight(72);
+}
+
+//Solucion al tercer ejercicio.
+penColour('#ff0000');
+for (var count = 0; count < 5; count++) {
+  moveForward(100);
+  turnRight(144);
+}
+
+//Solucion al cuarto ejercicio.
+penColour('#ff0000');
+for (var count = 0; count < 5; count++) {
+  moveForward(50);
+  turnRight(144);
+}
+penUp();
+moveForward(150);
+penDown();
+moveForward(20);
+
+turnRight(1);
+
+//Solucion al quinto ejercicio.
+You solved this level with 11 lines of JavaScript:
+penColour('#ff0000');
+for (var count2 = 0; count2 < 4; count2++) {
+  for (var count = 0; count < 5; count++) {
+    moveForward(50);
+    turnRight(144);
+  }
+  penUp();
+  moveForward(150);
+  penDown();
+  turnRight(90);
+}
+
+//Solucion al sexto ejercicio.
+penColour('#ff0000');
+for (var count2 = 0; count2 < 3; count2++) {
+  for (var count = 0; count < 5; count++) {
+    moveForward(50);
+    turnRight(144);
+  }
+  penUp();
+  moveForward(150);
+  penDown();
+  turnRight(120);
+}
+turnLeft(90);
+penUp();
+moveForward(100);
+penDown();
+moveForward(50);
+
+//Solucion al septimo ejercicio.
+penColour('#ff0000');
+for (var count2 = 0; count2 < 3; count2++) {
+  for (var count = 0; count < 5; count++) {
+    moveForward(50);
+    turnRight(144);
+  }
+  penUp();
+  moveForward(150);
+  penDown();
+  turnRight(120);
+}
+turnLeft(90);
+penUp();
+moveForward(100);
+penDown();
+for (var count3 = 0; count3 < 4; count3++) {
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  turnRight(45);
+}
+
+//Solucion al octavo ejercicio:
+penColour('#ff0000');
+for (var count2 = 0; count2 < 3; count2++) {
+  for (var count = 0; count < 5; count++) {
+    moveForward(50);
+    turnRight(144);
+  }
+  penUp();
+  moveForward(150);
+  penDown();
+  turnRight(120);
+}
+turnLeft(90);
+penUp();
+moveForward(100);
+penDown();
+for (var count3 = 0; count3 < 360; count3++) {
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  turnRight(1);
+}
+
+//Solucion al noveno ejericio. 
+penColour('#ff0000');
+for (var count2 = 0; count2 < 3; count2++) {
+  for (var count = 0; count < 5; count++) {
+    moveForward(50);
+    turnRight(144);
+  }
+  penUp();
+  moveForward(150);
+  penDown();
+  turnRight(120);
+}
+turnLeft(90);
+penUp();
+moveForward(100);
+penDown();
+for (var count3 = 0; count3 < 360; count3++) {
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  turnRight(1);
+}
+penColour('#000000');
+turnRight(120);
+moveForward(20);
+for (var count4 = 0; count4 < 360; count4++) {
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  moveForward(50);
+  turnRight(90);
+  turnRight(90);
+  turnRight(1);
+}
